04dee63e6dca39940f7c75d4a19be739d6b579f4
0e4ef622 <0e4ef622@gmail.com>
2016-05-19 16:34:38 -0500

    added option to see word and type definition

--- main.cpp
+++ main.cpp
@@ -23,12 +23,23 @@ std::string stringlower(std::string str) {
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " [cards-file]" << std::endl;
+        std::cout << "Usage: " << argv[0] << " [-w] [cards-file]\n-w shows the word and asks for the definition" << std::endl;
         return 1;
     }
 
+    bool word_first = false;
+
     std::ifstream cards_file;
-    cards_file.open(argv[1]);
+    if (std::string(argv[1]) == "-w") { // screw proper option parsing
+        if (argc > 2) {
+            word_first = true;
+            cards_file.open(argv[2]);
+        } else {
+            std::cout << "No cards-file specified" << std::endl;
+        }
+    } else {
+        cards_file.open(argv[2]);
+    }
 
     std::vector<card> cards;
     card cur;
@@ -47,21 +58,28 @@ int main(int argc, char *argv[]) {
         sort(cards.begin(), cards.end());
 
         for (card &v : cards ) {
-            std::cout << v.def << std::endl;
+            std::string word = v.word;
+            std::string def = v.def;
+            if (word_first) {
+                def = v.word;
+                word = v.def;
+            }
+
+            std::cout << def << std::endl;
 
             std::string answer;
             getline(std::cin, answer);
 
-            if (stringlower(answer) == stringlower(v.word)) {
+            if (stringlower(answer) == stringlower(word)) {
                 std::cout << "Correct" << std::endl << std::endl;
                 v.streak++;
             } else {
                 num_wrong++;
-                std::cout << "Wrong, the correct answer is " << v.word << std::endl;
+                std::cout << "Wrong, the correct answer is " << word << std::endl;
                 std::cout << "Type in the correct answer: ";
 
                 getline(std::cin, answer);
-                if (stringlower(answer) != stringlower(v.word)) {
+                if (stringlower(answer) != stringlower(word)) {
                     std::cout << "You had one job..." << std::endl;
                     failed_one_job++;
                 }
@@ -87,9 +105,26 @@ int main(int argc, char *argv[]) {
         }
 
         std::string input;
-        std::cout << "Try again? (y/n) ";
+http://xkcd.com/292/
+        std::cout << "Try again? (y/d/w/n/?) ";
         getline(std::cin, input);
-        if (tolower(input[0]) != 'y') loop = false;
+        switch (tolower(input[0])) {
+            case 'd':
+              word_first = false;
+              break;
+
+            case 'w':
+              word_first = true;
+              break;
+
+            case '?':
+              std::cout << "y = yes\nd = see definition first\nw = see word first\nn = no\n? = help\n";
+              goto http; // this was an terrible idea
+
+            default:
+              loop = false;
+              break;
+        }
     }
 
     return 0;
